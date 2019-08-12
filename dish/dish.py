888af78dbf6ed0c658d42c3a2dcb0a4df765466a
Mark E. Haase <mehaase@gmail.com>
2019-08-12 14:32:48 -0400

    Implement hostname checking
    The ignored hosts are set in the environment and a boolean indicates
    whether the host should be reported. Currently there is no reporting
    but I'm going to look into something on AWS to handle reports.

--- dish/dish.py
+++ dish/dish.py
@@ -1,7 +1,17 @@
+import os
+
 from flask import Flask, request
 app = Flask(__name__)
+ignore_hosts = os.getenv('DISH_IGNORE_HOSTS', '').lower().split(',')
+print('Ignoring hosts:')
+for host in ignore_hosts:
+    print('>', host)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def dish(path):
-    return 'Host: ' + request.headers['HOST']
+    host = request.headers.get('HOST', '').lower()
+    report = host not in ignore_hosts
+    reported = 'reported' if report else 'not reported'
+    return f'Your host is "{host}" ({reported})'
